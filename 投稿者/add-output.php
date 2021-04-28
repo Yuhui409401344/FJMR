00f07a38da9bc3dd0011748af0b356918c8e25bf
julieblue331 <julieblue331@gmail.com>
2021-04-28 15:09:45 +0800

    檔案檔名

--- 投稿者/add-output.php
+++ 投稿者/add-output.php
@@ -71,14 +71,19 @@
                                             <td><font color=navy><?php echo implode(',',$field) ?></font></td>
                                         </div>
                                     <?php
+                                        $filename=$_FILES["file"]["name"];
+                                        $name= explode('.',$filename);
+                                        $newname=$title.'r'.$row["count"].'.'.$name[1];
+                                        $odlname=$_FILES["file"]["tmp_name"];
+
                                         if ($_FILES["file"]["error"] > 0){
                                             echo "Error: " . $_FILES["file"]["error"];
                                         }else{
-                                            echo "檔案名稱: " . $row["count"].$_FILES["file"]["name"]."<br/>";
+                                            echo "檔案名稱: " . $newname."<br/>";
                                             echo "檔案類型: " . $_FILES["file"]["type"]."<br/>";
                                             echo "檔案大小: " . ($_FILES["file"]["size"] / 1024)." Kb<br />";
                                             echo "暫存名稱: " . $_FILES["file"]["tmp_name"];
-                                            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/".$row["count"].$_FILES["file"]["name"]);
+                                            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/".$newname);
                                         }
                                     ?> 
 
