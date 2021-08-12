60dc2376ca377588dbb852f1254451b3eb276df2
julieblue331 <julieblue331@gmail.com>
2021-08-12 14:34:45 +0800

    管理者

--- 管理者/reply.php
+++ 管理者/reply.php
@@ -1,3 +1,13 @@
+<?php 
+session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=fjup;charset=utf8', 'root', '');
+if(isset($_SESSION["account"]["login"])){
+    $manager=$_SESSION["account"]["login"];
+    foreach ($pdo->query("select status from account where login= '".$manager."'") as $row) {
+    $status[] = $row['status'];
+    }
+    if(in_array("管理者",$status)){
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -202,3 +212,11 @@
         
     </body>
 </html>
+<?php
+    }else{
+        include "pages-404.html";
+    }
+}else{
+    include "pages-404.html";
+}
+?>
