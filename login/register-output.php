70fd3be89456c2b7a47e4c85f35ffbb3d6d064eb
paggie chen <paggiechen8866@gmail.com>
2021-08-25 21:06:45 +0800

    update home photo and signup

--- login/register-output.php
+++ login/register-output.php
@@ -3,14 +3,20 @@ $name=$_POST["name"];
 $login=$_POST["login"];
 $password=$_POST["password"];
 $email=$_POST["email"];
+$school=$_POST["school"];
+$tel=$_POST["tel"];
+
 
 $pdo=new PDO('mysql:host=localhost;dbname=fjup;charset=utf8','root', '');
-$sql=$pdo->prepare('insert into account (name,login,password,email,status) VALUES(?,?,?,?,?)');
-$sql->execute([$name,$login,$password,$email,"投稿者"]);
+$sql=$pdo->prepare('insert into account (name,login,password,email,status,school) VALUES(?,?,?,?,?,?)');
+$sql->execute([$name,$login,$password,$email,"投稿者",$school]);
 $sql2=$pdo ->prepare('INSERT INTO account_field (login, f_name) VALUES (?,?)');
 $sql2->execute([$login,""]);
+$sql3=$pdo ->prepare('INSERT INTO account_tel (login, tel) VALUES (?,?)');
+$sql3->execute([$login,$tel]);
 
-$rs=$pdo->query('SELECT login FROM account');
+
+$rs=$pdo->query("SELECT login FROM account where status='投稿者'");
 if($login!=$rs)
 {
     header('location:success.php');
--- login/register.php
+++ login/register.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Register & Signup</title>
+        <title>Signup</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
@@ -20,99 +20,171 @@
 		<!-- icons -->
 		<link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 
+        <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f1f1f1;
+}
+
+#regForm {
+  background-color: #ffffff;
+  margin: 100px auto;
+  /* font-family: Raleway; */
+  padding: 40px;
+  width: 70%;
+  min-width: 300px;
+}
+
+h1 {
+  text-align: center;  
+}
+
+input {
+  padding: 10px;
+  width: 100%;
+  font-size: 17px;
+  /* font-family: Raleway; */
+  border: 1px solid #aaaaaa;
+}
+
+/* Mark input boxes that gets an error on validation: */
+input.invalid {
+  background-color: #ffdddd;
+}
+
+/* Hide all steps by default: */
+.tab {
+  display: none;
+}
+
+button {
+  background-color: #045aaa;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 17px;
+  /* font-family: Raleway; */
+  cursor: pointer;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+#prevBtn {
+  background-color: #bbbbbb;
+}
+
+/* Make circles that indicate the steps of the form: */
+.step {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbbbbb;
+  border: none;  
+  border-radius: 50%;
+  display: inline-block;
+  opacity: 0.5;
+}
+
+.step.active {
+  opacity: 1;
+}
+
+/* Mark the steps that are finished and valid: */
+.step.finish {
+  background-color: #045aaa;
+}
+</style>
+
     </head>
 
     <body class="loading authentication-bg authentication-bg-pattern">
-        <div class="account-pages mt-5 mb-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
-                        <div class="card bg-pattern">
+        <div class="account-pages mt-5">
+          <div class="container">
+            <form id="regForm" action="register-output.php" method="post">
+              <div class="auth-logo">
+                    <a href="../home/home.php" class="logo logo-dark text-center">
+                        <span class="logo-lg">
+                            <img src="../assets/images/logo-login.png" alt="" height="62">
+                        </span>
+                    </a>
 
-                            <div class="card-body p-4">
-                                
-                                <div class="text-center w-75 m-auto">
-                                    <div class="auth-logo">
-                                        <a href="../home/home.php" class="logo logo-dark text-center">
-                                            <span class="logo-lg">
-                                                <img src="../assets/images/logo-login.png" alt="" height="62">
-                                            </span>
-                                        </a>
-                    
-                                        <a href="../home/home.php" class="logo logo-light text-center">
-                                            <span class="logo-lg">
-                                                <img src="../assets/images/logo-login.png" alt="" height="62">
-                                            </span>
-                                        </a>
-                                    </div>
-                                    <p class="text-muted mb-4 mt-3">還沒註冊過嗎? 立刻創造一個吧~（預設身份為投稿者）</p>
-                                </div>
-        <form method="post" action="register-output.php">
-            <input type="hidden" name="method">
-                                    <div class="form-group">
-                                        <label for="name">姓名</label>
-                                        <input class="form-control" type="text" id="name" placeholder="請輸入姓名" required name="name">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="login">帳號</label>
-                                        <input class="form-control" type="text" id="login" required placeholder="請輸入帳號" name="login">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password">密碼</label>
-                                        <div class="input-group input-group-merge">
-                                            <input type="password" id="password" class="form-control" placeholder="請輸入密碼" name="password">
-                                            <div class="input-group-append" data-password="false">
-                                                <div class="input-group-text">
-                                                    <span class="password-eye"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                     <div class="form-group">
-                                        <label for="email">email</label>
-                                        <input class="form-control" type="email" id="email" required placeholder="請輸入信箱" name="email">
-                                    </div>                                   
-                                    <!-- <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="checkbox-signup">
-                                            <label class="custom-control-label" for="checkbox-signup">我同意 <a href="javascript: void(0);" class="text-dark">條款和條約</a></label>
-                                        </div>
-                                    </div> -->
-                                    <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-success btn-block" type="submit">註冊</button>
-                                    </div>
-
-                                </form>
-
-                                <!-- <div class="text-center">
-                                    <h5 class="mt-3 text-muted">其他註冊</h5>
-                                    <ul class="social-list list-inline mt-3 mb-0">
-                                        <li class="list-inline-item">
-                                            <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
-                                        </li>
-                                    </ul>
-                                </div> -->
-
-                            </div> <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
-
-
-
-                    </div> <!-- end col -->
+                    <a href="../home/home.php" class="logo logo-light text-center">
+                        <span class="logo-lg">
+                            <img src="../assets/images/logo-login.png" alt="" height="62">
+                        </span>
+                    </a>
                 </div>
-                <!-- end row -->
+                <p style="text-align:center">歡迎加入輔仁管理評論投稿者的行列</p>
+              <!-- One "tab" for each step in the form: -->
+              <div class="tab">
+                <div class="form-group">
+                  <label for="name">姓名</label>
+                  <input class="form-control " type="text" id="name" placeholder="請輸入姓名" required name="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input class="form-control" type="email" id="email" required placeholder="請輸入Email" name="email">
+                </div>      
+              </div>
+
+              <div class="tab">
+                <div class="form-group">
+                  <label for="login">帳號</label>
+                  <input class="form-control" type="text" id="login" required placeholder="請輸入帳號" name="login">
+                </div>
+                <div class="form-group">
+                    <label for="password">密碼</label>
+                    <div class="input-group input-group-merge">
+                        <input type="password" id="password" class="form-control" placeholder="請輸入密碼" name="password">
+                        <div class="input-group-append" data-password="false">
+                            <div class="input-group-text">
+                                <span class="password-eye"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <div class="tab">
+                <div class="form-group">
+                    <label for="school">學校</label>
+                    <input class="form-control" type="text" id="school" required placeholder="請輸入您所在的學校" name="school">
+                </div>  
+                <div class="form-group">
+                    <label for="tel">連絡電話</label>
+                    <input class="form-control" type="text" id="tel" required placeholder="請輸入您的學校電話" name="tel">
+                </div>   
+              </div>
+             
+              <div style="overflow:auto;">
+                <div style="float:right;">
+                  <button type="button" id="prevBtn" onclick="nextPrev(-1)">前一步</button>
+                  <button type="button" id="nextBtn" onclick="nextPrev(1)">下一步</button>
+                </div>
+              </div>
+              <!-- Circles which indicates the steps of the form: -->
+              <div style="text-align:center;margin-top:40px;">
+                <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
+              </div>
+              
             </div>
-            <!-- end container -->
+            <!-- end row -->
+            <div class="row">
+                  <div class="col-12 text-center">
+                      <p class="text-white-50">已經有投稿者帳號了嗎？<a href="login.php" class="text-white ml-1"><b>登入</b></a></p>
+                  </div> <!-- end col -->
+              </div>
+              <!-- end row -->
+            </form>
+           
+          </div>
+        
         </div>
         <!-- end page -->
 
@@ -122,6 +194,81 @@
 
         <!-- App js -->
         <script src="../assets/js/app.min.js"></script>
+
+        <script>
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "下一步";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+</script>
         
     </body>
 </html>
