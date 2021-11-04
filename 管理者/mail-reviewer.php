7cf59ad1d8a906797dbf0c6745ef868ba536f64c
paggie chen <paggiechen8866@gmail.com>
2021-11-04 13:52:56 +0800

    update

--- 管理者/mail-reviewer.php
+++ 管理者/mail-reviewer.php
@@ -1,6 +1,8 @@
 <?php 
 
-$slogan="Today's research, tomorrow's innovation. ";
+$slogan="Today's research, tomorrow's innovation.";
+
+
 
 $emailContent = 
 '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -83,12 +85,15 @@ $emailContent =
           text-decoration: none !important;
         }
 
+        .yesnoBtn {
+          font-size: 22px; line-height: 36px; width:80px; font-family: Cabin, sans-serif; border:0; color: #FFFFFF;
+        }
+
     </style>
 
   <link href="https://fonts.googleapis.com/css?family=Cabin:400,700&display=swap" rel="stylesheet" type="text/css"><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet" type="text/css"><!--<![endif]-->
 
 </head>
-
 
 <body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;color: #000000">
   <!--[if IE]><div class="ie-container"><![endif]-->
@@ -112,10 +117,9 @@ $emailContent =
                         <tr>
                           <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:50px 10px 15px 30px;font-family:arial,helvetica,sans-serif;" align="left">
                             
-                      <div class="v-text-align" style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
-                        <p style="font-size: 36px; line-height: 140%;">輔仁管理評論</p>
-                      </div>
-
+                          <div class="v-text-align" style="color: #ffffff; line-height: 140%; text-align: left; word-wrap: break-word;">
+                          <p style="font-size: 36px; line-height: 140%;">輔仁管理評論</p>
+                        </div>
                           </td>
                         </tr>
                       </tbody>
@@ -145,57 +149,64 @@ $emailContent =
                 <div class="u-col u-col-100" style="max-width: 320px;min-width: 550px;display: table-cell;vertical-align: top;">
                   <div style="width: 100% !important;">
                     <table id="u_content_heading_1" style="font-family:arial,helvetica,sans-serif; background-color: #eceff1;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-                    <tbody>
-                        <tr>
-                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:50px 44px 15px;font-family:arial,helvetica,sans-serif;" align="left">
-                      <p style="font-family: "Open Sans",sans-serif; ">親愛的管理者，'.$managerName.'（女士/先生）您好：審稿者'.$yourName.'（女士/先生）已回覆《'.$title.'》,請您上平台查看詳細的回覆檔案。</p>
-                          </td>
-                          
-                        </tr>
-                        <tr>
-                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:15px 44px 5px;font-family:arial,helvetica,sans-serif;" align="left">
-                            <p style="font-family: "Open Sans",sans-serif; ">回覆意見：'.$level.'</p></br>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:15px 44px 5px;font-family:arial,helvetica,sans-serif;" align="left">
-                            <p style="font-family: "Open Sans",sans-serif; ">回覆留言：'.$comment.'</p></br>
-                          </td>
-                        </tr>
-                      </tbody>  
-                   
-                    </table>
-                    
-                    <table style="font-family:arial,helvetica,sans-serif; background-color: #eceff1;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
                       <tbody>
                         <tr>
-                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:30px 10px 60px;font-family:arial,helvetica,sans-serif;" align="left">
-                            
-                            <div class="v-text-align" align="center">
-                              <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;font-family:arial,helvetica,sans-serif;"><tr><td class="v-text-align" style="font-family:arial,helvetica,sans-serif;" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://fjmr.fju.edu.tw/login/login.php" style="height:52px; v-text-anchor:middle; width:318px;" arcsize="4%" stroke="f" fillcolor="#d36c09"><w:anchorlock/><center style="color:#FFFFFF;font-family:arial,helvetica,sans-serif;"><![endif]-->
-                                <a href="http://fjmr.fju.edu.tw/login/login.php" target="_blank" style="box-sizing: border-box;display: inline-block;font-family:arial,helvetica,sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #d36c09; border-radius: 2px; -webkit-border-radius: 2px; -moz-border-radius: 2px; width:60%; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;">
-                                  <span style="display:block;padding:15px 40px;line-height:120%;"><span style="font-size: 18px; line-height: 21.6px; font-family: Cabin, sans-serif;">Go to FJMR dashboard</span></span>
-                                </a>
-                              <!--[if mso]></center></v:roundrect></td></tr></table><![endif]-->
-                            </div>
-
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:50px 44px 15px;font-family:arial,helvetica,sans-serif;" align="left">
+                            <p style="font-family: "Open Sans",sans-serif; ">親愛的審稿者，'.$name.'（女士/先生)您好：輔仁管理評論現有一封分配給您的稿件《'.$title.'》，待您審查，還請您決定是否接收！</p>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:15px 44px 15px;font-family:arial,helvetica,sans-serif;" align="left">
+                            <p style="font-family: "Open Sans",sans-serif; text-align:justify;">摘要：'.$summary.'</p>
                           </td>
                         </tr>
                       </tbody>
                     </table>
-
+                    <table style="font-family:arial,helvetica,sans-serif; background-color: #eceff1;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                    <tbody>
+                      <tr>
+                        <td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:30px 10px 60px;font-family:arial,helvetica,sans-serif;" align="left">
                           
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</body>
+                          <div class="v-text-align" align="center">
+                            <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="border-spacing: 0; border-collapse: collapse; mso-table-lspace:0pt; mso-table-rspace:0pt;font-family:arial,helvetica,sans-serif;"><tr><td class="v-text-align" style="font-family:arial,helvetica,sans-serif;" align="center"><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://fjmr.fju.edu.tw/login/login.php" style="height:52px; v-text-anchor:middle; width:318px;" arcsize="4%" stroke="f" fillcolor="#d36c09"><w:anchorlock/><center style="color:#FFFFFF;font-family:arial,helvetica,sans-serif;"><![endif]-->
+                             
+<a href="140.136.9.232/webhw/管理者/accept.php?accept=0&id='.$id.'&reviewer='.$a.'"
+    style="box-sizing: border-box;display: inline-block;font-family:arial,helvetica,sans-serif;text-decoration: none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #808080; border-radius: 2px; -webkit-border-radius: 2px; -moz-border-radius: 2px; width:80%; max-width:100%; overflow-wrap: break-word; word-break: break-word; word-wrap:break-word; mso-border-alt: none;">
+    <span style="display:block;padding:15px 40px;line-height:120%;"><span
+            style="font-size: 18px; line-height: 21.6px; font-family: Cabin, sans-serif;">No</span></span>
+</a>
+</div>
 
+</td>
+<td class="v-container-padding-padding" style="overflow-wrap:break-word;word-break:break-word;padding:30px 10px 60px;font-family:arial,helvetica,sans-serif;" align="left">
+                          
+                          <div class="v-text-align" align="center">
+                              <a href="140.136.9.232/webhw/管理者/accept.php?accept=1&id='.$id.'&reviewer='.$a.'" style="box-sizing: border-box;display: inline-block;font-family:arial,helvetica,sans-serif;text-decoration:
+none;-webkit-text-size-adjust: none;text-align: center;color: #FFFFFF; background-color: #d36c09; border-radius: 2px;
+-webkit-border-radius: 2px; -moz-border-radius: 2px; width:80%; max-width:100%; overflow-wrap: break-word; word-break:
+break-word; word-wrap:break-word; mso-border-alt: none;">
+<span style="display:block;padding:15px 40px;line-height:120%;"><span
+        style="font-size: 18px; line-height: 21.6px; font-family: Cabin, sans-serif;">Yes</span></span>
+</a>
+
+</div>
+
+</td>
+</tr>
+</tbody>
+</table>
+
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</body>
 
 </html>
 
